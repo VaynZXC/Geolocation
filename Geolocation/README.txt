b668828403d76950cd3a1474aa024b1bafcb2109
VaynZXC <126894971+VaynZXC@users.noreply.github.com>
2023-07-07 22:49:57 +0300

    Add files via upload

---
+++ Geolocation/README.txt
@@ -0,0 +1,3 @@
+A training site that helps you track your location.
+
+Click on the button, give permission and get your location on the map in response.
---
+++ Geolocation/index.js
@@ -0,0 +1,28 @@
+const btn = document.querySelector('.j-btn-test');
+const mapLink = document.querySelector('#map-link')
+const status = document.querySelector('#status')
+
+const error = () => {
+  status.textContent = 'Невозможно получить ваше метоположение';
+}
+
+const sucess = (position) => {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+
+  status.textContent = `Широта: ${latitude}, Долгота: ${longitude}`;
+  mapLink.href =  `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+  mapLink.textContent = 'Ссылка на карту';
+}
+
+btn.addEventListener('click', () => {
+  mapLink.href = '';
+  mapLink.textContent = '';
+
+  if (!navigator.geolocation) {
+    status.textContent = 'Geolocation не поддерживается вашим бораузеров';
+  } else {
+    status.textContent = 'Определение местоположения...';
+    navigator.geolocation.getCurrentPosition(sucess, error);
+  }
+}); 
